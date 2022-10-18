0508d0d9f4f8a7dc2193b4720f04a95036e66843
Mathieu Cantin <103442330+mcantinqc@users.noreply.github.com>
2022-10-17 22:31:21 -0400

    Add livecycle

--- main.tf
+++ main.tf
@@ -139,6 +139,12 @@ resource "aws_instance" "this" {
 
   tags        = merge({ "Name" = var.name }, var.tags)
   volume_tags = var.enable_volume_tags ? merge({ "Name" = var.name }, var.volume_tags) : null
+
+  lifecycle {
+    ignore_changes = [
+      ami,
+    ]
+  }
 }
 
 resource "aws_spot_instance_request" "this" {
